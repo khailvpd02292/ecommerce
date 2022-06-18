575c3f5899706ac289ceceb242d3c8003d06eb06
KhaiLe <khailv@dn.paracelsoft.com>
2022-06-18 12:29:59 +0700

    fixbug export excel

--- app/Http/Controllers/Admin/ExportController.php
+++ app/Http/Controllers/Admin/ExportController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\BaseController;
 use Illuminate\Http\Request;
 use Maatwebsite\Excel\Facades\Excel;
 use App\Exports\ExportOrder;
@@ -10,18 +10,20 @@ use Validator;
 use Symfony\Component\HttpFoundation\Response;
 
 
-class ExportController extends Controller
+class ExportController extends BaseController
 {
 
     public function export(Request $request) {
 
-        $validator = Validator::make($request->all(), [
-            'status' => 'required|integer|min:0|max:3',
-        ]);
+        if (isset($request->status)) {
+            $validator = Validator::make($request->all(), [
+                'status' => 'required|integer|min:0|max:3',
+            ]);
 
-        if ($validator->fails()) {
-            
-            return $this->sendError($validator->errors()->first(), Response::HTTP_BAD_REQUEST);
+            if ($validator->fails()) {
+
+                return $this->sendError($validator->errors()->first(), Response::HTTP_BAD_REQUEST);
+            }
         }
 
         return Excel::download(new ExportOrder($request->status), date('Ymd_Hsi').'_order.xlsx');
